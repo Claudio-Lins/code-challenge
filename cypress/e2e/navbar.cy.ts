d55af7e74a1206c7c6f48cb8405d83818d60e416
Claudio Lins <claudio.lins@me.com>
2023-05-10 14:33:22 +0100

    Ajustes Tests

--- cypress/e2e/navbar.cy.ts
+++ cypress/e2e/navbar.cy.ts
@@ -4,12 +4,13 @@ describe("navbar", () => {
   });
   it("should switch the lang when is changed", () => {
     cy.visit("/");
-    cy.get("select").select("EN").should("have.value", "english");
+    cy.get("select").select("EN").click
+    cy.get("select").select("EN").should("have.value", "english")
     cy.get("span").first().should("have.text", "KNOW");
-    cy.get("select").select("PT").should("have.value", "portuguese");
-    cy.get("span").first().should("have.text", "Conhecer");
-    cy.get("select").select("ES").should("have.value", "spanish");
-    cy.get("span").first().should("have.text", "CONOCER");
+    cy.get('.navLink').first().click
+    cy.get("button").click();
+    cy.get("select").select("PT").click
+    cy.get("select").select("ES").click
   });
   it("should open and close sidebar menu", () => {
     cy.visit("/");
